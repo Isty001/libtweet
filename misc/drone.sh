a29b6200fe49d212ada9d5972d6e50900c7ce165
Isty001 <isty001@gmail.com>
2016-07-10 22:27:52 +0200

    drone.io

--- misc/drone.sh
+++ misc/drone.sh
@@ -3,8 +3,9 @@
 sudo rm -f /etc/apt/sources.list.d/google-chrome.list
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update || true
-sudo apt-get install gcc-5 -y
+sudo apt-get install gcc-5
 sudo apt-get install curl
 sudo apt-get install liboauth-dev
+sudo apt-get install libcmocka0
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20
-make test-unit
+sudo make test-unit
